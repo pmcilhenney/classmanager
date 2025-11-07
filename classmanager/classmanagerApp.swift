8d937b6f547e79fb04c6b8be4f9eca4d3ebf9a6f
Patrick McIlhenney <pmcilhenney@gcemstrainingacademy.org>
2025-11-07 11:15:46 -0500

    Initial Commit

---
+++ classmanager/classmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  classmanagerApp.swift
+//  classmanager
+//
+//  Created by Patrick McIlhenney on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct classmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ classmanagerTests/classmanagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  classmanagerTests.swift
+//  classmanagerTests
+//
+//  Created by Patrick McIlhenney on 11/7/25.
+//
+
+import Testing
+@testable import classmanager
+
+struct classmanagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
