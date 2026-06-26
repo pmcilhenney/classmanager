f8ff8207a0e00ccc25d9ce247f409f9ce06afb13
Patrick McIlhenney <pmcilhenney@gcemstrainingacademy.org>
2026-06-26 14:11:32 -0400

    Update FlexiQuiz mini exam catalog

--- classmanager/QuizModels.swift
+++ classmanager/QuizModels.swift
@@ -20,22 +20,22 @@ struct QuizInfo: Identifiable {
                 url: URL(string: "https://www.flexiquiz.com/SC/N/66564166-9de9-4b17-9c2d-6f76bc186970")!
             ),
             QuizInfo(
-                id: "41b6732a-dbfd-4663-8265-484b7256e34f",
+                id: "78df99bd-d81a-4f24-a855-81ea0a3a71ec",
                 number: 2,
                 title: "Refresher A Mini-Quiz #2",
-                url: URL(string: "https://www.flexiquiz.com/SC/N/41b6732a-dbfd-4663-8265-484b7256e34f")!
+                url: URL(string: "https://www.flexiquiz.com/SC/N/78df99bd-d81a-4f24-a855-81ea0a3a71ec")!
             ),
             QuizInfo(
-                id: "42797c34-bbe7-4048-9b04-b206d6663e35",
+                id: "772e07cf-d20b-4c8e-a6a5-d2917f5aa5c7",
                 number: 3,
                 title: "Refresher A Mini-Quiz #3",
-                url: URL(string: "https://www.flexiquiz.com/SC/N/42797c34-bbe7-4048-9b04-b206d6663e35")!
+                url: URL(string: "https://www.flexiquiz.com/SC/N/772e07cf-d20b-4c8e-a6a5-d2917f5aa5c7")!
             ),
             QuizInfo(
-                id: "1295f5ad-3fdc-43a1-8386-037a5dee8ecc",
+                id: "16ca7d9a-d3a4-4a24-85fe-ccb49def519d",
                 number: 4,
                 title: "Refresher A Mini-Quiz #4",
-                url: URL(string: "https://www.flexiquiz.com/SC/N/1295f5ad-3fdc-43a1-8386-037a5dee8ecc")!
+                url: URL(string: "https://www.flexiquiz.com/SC/N/16ca7d9a-d3a4-4a24-85fe-ccb49def519d")!
             )
         ]
     }
@@ -43,28 +43,28 @@ struct QuizInfo: Identifiable {
     static func refresherBQuizzes() -> [QuizInfo] {
         [
             QuizInfo(
-            id: "a83694f9-baff-4d48-853e-653f5c0f8468",
+            id: "3eff7d7c-74d4-44d8-bb4f-b8561c0c62b8",
             number: 1,
             title: "Refresher B Mini-Quiz #1",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/a83694f9-baff-4d48-853e-653f5c0f8468")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/3eff7d7c-74d4-44d8-bb4f-b8561c0c62b8")!
         ),
         QuizInfo(
-            id: "0f051560-59f5-4812-8874-e44968104bbc",
+            id: "67ca0a1e-7c79-4ae7-aa55-418d10e9f3b5",
             number: 2,
             title: "Refresher B Mini-Quiz #2",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/0f051560-59f5-4812-8874-e44968104bbc")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/67ca0a1e-7c79-4ae7-aa55-418d10e9f3b5")!
         ),
         QuizInfo(
-            id: "b43eba66-0a3b-4136-ae95-4d4b806b36f7",
+            id: "e5fdb765-119b-4f5e-905b-c9b7d27ed2bb",
             number: 3,
             title: "Refresher B Mini-Quiz #3",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/b43eba66-0a3b-4136-ae95-4d4b806b36f7")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/e5fdb765-119b-4f5e-905b-c9b7d27ed2bb")!
         ),
         QuizInfo(
-            id: "86dfa414-1245-406b-be54-8572e5b82812",
+            id: "757c48dc-6ab2-4aad-a262-30ed854157c9",
             number: 4,
             title: "Refresher B Mini-Quiz #4",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/86dfa414-1245-406b-be54-8572e5b82812")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/757c48dc-6ab2-4aad-a262-30ed854157c9")!
         )
         ]
     }
@@ -72,28 +72,28 @@ struct QuizInfo: Identifiable {
     static func refresherCQuizzes() -> [QuizInfo] {
         [
             QuizInfo(
-            id: "dc9e0a85-4ac7-4418-b843-6a1c1edcfb5d",
+            id: "ab8a5c9d-9e06-42c2-a866-e5759d8b2209",
             number: 1,
             title: "Refresher C Mini-Quiz #1",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/dc9e0a85-4ac7-4418-b843-6a1c1edcfb5d")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/ab8a5c9d-9e06-42c2-a866-e5759d8b2209")!
         ),
         QuizInfo(
-            id: "797d9c0b-bb05-4273-b2ee-418a870b376b",
+            id: "d76f4483-d8cc-4029-aea6-a2bebbb3d086",
             number: 2,
             title: "Refresher C Mini-Quiz #2",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/797d9c0b-bb05-4273-b2ee-418a870b376b")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/d76f4483-d8cc-4029-aea6-a2bebbb3d086")!
         ),
         QuizInfo(
-            id: "99315211-1727-42cc-b7de-8b5c4843149b",
+            id: "b7adaf94-a911-4dad-8152-a5853cb02e35",
             number: 3,
             title: "Refresher C Mini-Quiz #3",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/99315211-1727-42cc-b7de-8b5c4843149b")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/b7adaf94-a911-4dad-8152-a5853cb02e35")!
         ),
         QuizInfo(
-            id: "0ce0a5ce-2e06-46dc-8915-310d008b6eed",
+            id: "b938cd8b-913c-41bf-b247-1406b11115f2",
             number: 4,
             title: "Refresher C Mini-Quiz #4",
-            url: URL(string: "https://www.flexiquiz.com/SC/N/0ce0a5ce-2e06-46dc-8915-310d008b6eed")!
+            url: URL(string: "https://www.flexiquiz.com/SC/N/b938cd8b-913c-41bf-b247-1406b11115f2")!
         )
             
             
